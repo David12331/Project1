93726ddba0789c1b1dce5e1c1aea4919c59e19e6
David12331 <dpantoja1@pride.hofstra.edu>
2023-10-31 15:37:56 -0400

    Timer function and Scramble Function

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log('jjj');
+/////funtions//////
+
+
+function startGame(){
+
+// Initializes the game    
+
+}
+
+function scrambleWord(){
+
+// Selects the word and puts the letters in Random Order
+
+}
+
+function unscrambleWord(){
+
+// Compares user input to the correct word
+
+}
+
+function hint(){
+
+// Provide a hint to the player if the player asks for it 
+
+}
+
+function timer(){
+
+// Counts down 30 seconds once every new word appears
+
+}
+
+function updateScore() {
+
+// Updates player score     
+
+}
+
+function endGame(){
+
+// Ends game and displays final score
+
+
+}
